8f246148ac7650ec566f1aa279309b1f580dc452
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-24 07:13:41 +0530

    Added Tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-  return <main className="flex flex-col h-screen ">
-    <h1>
-      Hii
-    </h1>
-  </main>;
+  return (
+    <main className="flex flex-col h-screen justify-center items-center py-6 px-8 ">
+      <h1 className="text-2xl text-red-600 font-semibold">Hii</h1>
+    </main>
+  );
 }
 
 export default App;
